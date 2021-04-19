d5848d9000e6188e07c3bf70af50c5cfeaed48e6
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-04-19 14:00:53 +0200

    change subscription working

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,6 @@ router.use(cors());
 const rand = require("random-key");
 var CryptoJS = require("crypto-js");
 
-const randomKey = rand.generate(7);
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -22,16 +20,17 @@ router.post("/login", (req, res) => {
     
     let users = JSON.parse(data);
     let foundUser = users.find(({username}) => username == req.body.username);
-    let originalPass = CryptoJS.AES.decrypt(foundUser.username, randomKey).toString(CryptoJS.encrypt.toString());
-
+    let originalPass = CryptoJS.AES.decrypt(foundUser.password, "nyckel").toString(CryptoJS.enc.Utf8);
+    console.log('originalpass', originalPass);
     const currentUser = {username: foundUser.username, id: foundUser.id};
-    console.log('currentUser', currentUser);
-    if(foundUser) {
-      if(originalPass == req.body.password) {
-        res.json(currentUser);
-      };
-    } else {
-      res.json("användare eller lösen stämmer inte")
+    if(foundUser){
+       if(originalPass == req.body.password) {
+      res.json(currentUser);
+    }
+   
+    }else {
+      console.log("else")
+     // res.json("användare eller lösen stämmer inte");
     };
 
   });
@@ -41,7 +40,7 @@ router.post("/login", (req, res) => {
 //CREATE NEW USER
 router.post("/createAccount", (req, res) => {
   
-  let randomId = rand.generateDigits(5);
+  let randomId = rand.generateDigits(8);
   
   if(req.body.subscribe) {
     fs.readFile("newsletter.json", (err, data) => {
@@ -62,7 +61,7 @@ router.post("/createAccount", (req, res) => {
     if(err) console.log(err);
     let users = JSON.parse(data);
 
-    let cryptPass = CryptoJS.AES.encrypt(req.body.password, randomKey).toString();
+    let cryptPass = CryptoJS.AES.encrypt(req.body.password, "nyckel").toString();
 
     let user = {
       username: req.body.username,
@@ -70,7 +69,8 @@ router.post("/createAccount", (req, res) => {
       lastname: req.body.lastname, 
       email: req.body.email,
       password: cryptPass,
-      id: randomId
+      id: randomId,
+      subscribe: req.body.subscribe
     };
   
     users.push(user);
@@ -87,7 +87,69 @@ router.post("/createAccount", (req, res) => {
 });
 
 
+//SEND USERS DETAILS FOR PRINT
+router.post('/myAccount/', function(req, res, next) {
 
+  fs.readFile("users.json", (err, data) => {
+    if(err) console.log(err);
+    let users = JSON.parse(data);
+
+    let foundUser = users.find(({id}) => id == req.body.id);
+ 
+    let user = {
+      "username": foundUser.username,
+      "firstname":foundUser.firstname, 
+      "lastname": foundUser.lastname, 
+      "email": foundUser.email,
+      "subscribe": foundUser.subscribe
+     };
+
+    res.json(user);  
+  });
+});
+
+
+
+//UPDATE SUBSCRIPTION
+router.post('/newsletter', function(req, res, next) {
+  console.log('req.body', req.body);
+  fs.readFile("users.json", (err, data) => {
+    if(err) console.log(err);
+    let users = JSON.parse(data);
+
+    let foundUser = users.find(({id}) => id == req.body.id);
+    
+    if(foundUser.subscribe != req.body.subscribe) {
+      foundUser.subscribe = req.body.subscribe;
+
+      fs.readFile("newsletter.json", (err, data) => {
+        if(err) console.log(err);
+         
+        let newsletter = JSON.parse(data); 
+          
+          if(foundUser.subscribe) {
+            newsletter.push(foundUser.email);
+            console.log("newsletter before push", newsletter)
+          } else {
+            let foundEmail = newsletter.find((newsletter) => newsletter == foundUser.email);
+            for( let email in newsletter) {
+              if (newsletter[email] === foundEmail) newsletter.splice(email, 1); 
+            }
+          }    
+          fs.writeFile("newsletter.json", JSON.stringify(newsletter, null, 2), (err) => {
+            if(err) console.log(err);
+          });
+      });
+     
+    }
+
+    fs.writeFile("users.json", JSON.stringify(users, null, 2), (err) => {
+      if(err) console.log(err);
+    });
+
+    res.json(foundUser.subscribe);  
+  });
+});
 
 module.exports = router;
 
