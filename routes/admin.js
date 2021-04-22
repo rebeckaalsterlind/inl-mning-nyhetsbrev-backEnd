383a076382ad158c415b82cc9c19963a0b5c4fc3
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-04-22 13:00:48 +0200

    connects to database

--- routes/admin.js
+++ routes/admin.js
@@ -12,26 +12,32 @@ router.get('/', function(req, res, next) {
 });
 
 
+
+// //SPARA I DB
+// router.post("/add", function(req, res) {
+//     req.app.locals.db.collection("users").insertOne(req.body)
+//     .then(result => {
+//         console.log(result)
+//         res.redirect("/show");
+//     })
+// })
+
+
+//HÄMTA FRÅN DB
+//let findUser = req.body.username ?? 
+
     
 router.post("/", (req, res) => {
     console.log('rew.boydy', req.body)
     
-    fs.readFile("admin.json", (err, data) => {
-    if(err) console.log(err);
-    let admin = JSON.parse(data);
-  
-    let foundAdmin = admin.find(({username}) => username == req.body.username);
-
-    if(foundAdmin.password === req.body.password) {
-        
-        fs.readFile("users.json", (err, data) => {
-
-            let printUsers = `<div><h2>All members</h2>`
-            if(err) console.log(err);
-            let users = JSON.parse(data);
-           
-            for (let user in users) {
-                printUsers += `
+    req.app.locals.db.collection("admin").find({"username": req.body.username}).toArray()
+    .then(result => {
+        if(result[0].password === req.body.password){
+           req.app.locals.db.collection("users").find().toArray()
+           .then( users => {
+            let printLists = `<div><h2>All Members</h2>`
+            for (let user in users)
+                printLists += `
                 <div>
                 <p>Username: ${users[user].username}</p> 
                 <p>Name: ${users[user].firstname} ${users[user].lastname}</p> 
@@ -39,25 +45,72 @@ router.post("/", (req, res) => {
                 <p>Newsletter: ${(users[user].subscribe) ? "subscribing" : "not subscribing"}</p> 
                 
                 </div><br /><br />`;
-            };    
-            
-            fs.readFile("newsletter.json", (err, data) => {
-            if(err) console.log(err);
-            let subscribers = JSON.parse(data);
-            printUsers += `<div>Subscribers</div>`;
+
+                printLists += `</div>`;
+
+                req.app.locals.db.collection("newsletter").find().toArray()
+                .then( subscribers => {
+                    
+                    printLists += `<div><h2>Subscribers</h2>`
+
+                    for (let subscriber in subscribers) {
+                    printLists += `<p>${subscribers[subscriber].email}</p>`    
+                    }
+
+                    printLists += `</div>`;
+
+                    res.send(printLists)
+                });
+           });
+
+        } else {
+            res.send("username or password is invalid")
+        }
+ 
+    })
+
+    // fs.readFile("admin.json", (err, data) => {
+    // if(err) console.log(err);
+    // let admin = JSON.parse(data);
+  
+    // let foundAdmin = admin.find(({username}) => username == req.body.username);
+
+    // if(foundAdmin.password === req.body.password) {
+        
+    //     fs.readFile("users.json", (err, data) => {
+
+            // let printUsers = `<div><h2>All members</h2>`
+            // if(err) console.log(err);
+            // let users = JSON.parse(data);
            
-            for (subscriber in subscribers) {
-                printUsers += `
-                <div>${subscribers[subscriber]}</div>`;
-            };
-            printUsers += `</div>`;
-            res.send(printUsers)  
-        });         
-        });
-    } else {
-        res.redirect("/admin/noAccess")
-    }
-    });
+            // for (let user in users) {
+            //     printUsers += `
+            //     <div>
+            //     <p>Username: ${users[user].username}</p> 
+            //     <p>Name: ${users[user].firstname} ${users[user].lastname}</p> 
+            //     <p>Email: ${users[user].email}</p> 
+            //     <p>Newsletter: ${(users[user].subscribe) ? "subscribing" : "not subscribing"}</p> 
+                
+            //     </div><br /><br />`;
+            // };    
+            
+    //         fs.readFile("newsletter.json", (err, data) => {
+    //         if(err) console.log(err);
+    //         let subscribers = JSON.parse(data);
+    //         printUsers += `<div>Subscribers</div>`;
+           
+    //         for (subscriber in subscribers) {
+    //             printUsers += `
+    //             <div>${subscribers[subscriber]}</div>`;
+    //         };
+    //         printUsers += `</div>`;
+    //         res.send(printUsers)  
+    //     });         
+    //     });
+    // } else {
+    //     res.redirect("/admin/noAccess")
+    // }
+    // });
 
 });
 
