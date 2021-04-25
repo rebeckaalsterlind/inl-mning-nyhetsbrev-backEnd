a4270863d1a16e25d75a6dc3e326803d06d6674a
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-04-25 13:50:46 +0200

    -

--- routes/users.js
+++ routes/users.js
@@ -86,7 +86,8 @@ router.post("/createAccount", (req, res) => {
 
 //SEND USERS DETAILS FOR PRINT
 router.post('/myAccount/', function(req, res, next) {
-  req.app.locals.db.collection("users").find({"_id": req.body._id}).toArray()
+  req.app.locals.db.collection("users").find({"_id": req.body.id}).toArray()
+  console.log("id", req.body.id)
   .then(result => {
 
     let user = {
